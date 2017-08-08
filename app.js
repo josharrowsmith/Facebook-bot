c3a463a4781ff0f296a943eafea1eb428c1c0350
Josh <josh.arrowsmith@hotmail.com>
2017-08-08 16:11:39 +1000

    work

--- app.js
+++ app.js
@@ -149,7 +149,9 @@ function getMovieDetail(userId, field) {
 }
 
 function findMovie(userId, movieTitle) {
-  request("http://www.omdbapi.com/?t=" + movieTitle + "&apikey=7e0bbc93", function (error, response, body) {
+  //https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+  //"http://www.omdbapi.com/?t=" + movieTitle + "&apikey=7e0bbc93"
+  request("https://api.themoviedb.org/3/search/movie?api_key=1ac2e46af3c6445c8f78d27dd6debcc1&query=" +  movieTitle,  function (error, response, body) {
     if (error || response.statusCode !== 200) {
       return sendMessage(userId, { text: `Something went wrong. Try again.` });
     }
