e5b5bfafe6422ebd6598c138a244c30871a58380
Josh <josh.arrowsmith@hotmail.com>
2017-08-09 11:31:30 +1000

    work

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ function getMovieDetail(userId, field) {
 function findMovie(userId, movieTitle) {
   //https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
   //"http://www.omdbapi.com/?t=" + movieTitle + "&apikey=7e0bbc93"
-  request("https://api.themoviedb.org/3/search/movie" +"api_key=1ac2e46af3c6445c8f78d27dd6debcc1" + &query= +  movieTitle,  function (error, response, body) {
+  request("https://api.themoviedb.org/3/search/movie?&query=" +  movieTitle,  function (error, response, body) {
     if (error || response.statusCode !== 200) {
       return sendMessage(userId, { text: `Something went wrong. Try again.` });
     }
