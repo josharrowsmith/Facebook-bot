1f0d784707bdce3984b64c267dbd3a760c366f4f
Josh <josh.arrowsmith@hotmail.com>
2017-08-12 18:39:08 +1000

    work

--- app.js
+++ app.js
@@ -183,9 +183,9 @@ function findMovie(userId, movieTitle) {
             payload: {
               template_type: 'generic',
               elements: [{
-                title: themoviedb.original_title,
+                title: 'batman',
                 subtitle: 'Is this the movie you are looking for?',
-                image_url: themoviedb.poster_path === 'N/A' ? 'http://placehold.it/350x150' : themoviedb.poster_path,
+                image_url: ,
                 buttons: [{
                   type: 'postback',
                   title: 'Yes',
