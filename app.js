3d3ac0c3a23f9892b87c33bb379fa0bc6d708a27
Josh <josh.arrowsmith@hotmail.com>
2017-08-12 19:25:04 +1000

    work

--- app.js
+++ app.js
@@ -183,7 +183,7 @@ function findMovie(userId, movieTitle) {
             payload: {
               template_type: 'generic',
               elements: [{
-                title: themoviedb.results.original_title,
+                title: "batman",
                 subtitle: 'Is this the movie you are looking for?',
                 image_url: "http://seinfeld-v1.tripod.com/pictures/kramer/seinfeld_tkramer4.jpg",
                 buttons: [{
