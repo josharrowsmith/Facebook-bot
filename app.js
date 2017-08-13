8fa23bcb673eac0cbfa811b7d3eb879017c9e24b
Josh <josh.arrowsmith@hotmail.com>
2017-08-13 15:59:38 +1000

    work

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ function findMovie(userId, movieTitle) {
               elements: [{
                 title:themoviedb.results[0].original_title ,
                 subtitle: 'Is this the movie you are looking for?',
-                image_url: "https://image.tmdb.org/t/p/w500/"+ themoviedb.results[0].poster_path === 'N/A' ? 'http://placehold.it/400x400' : "https://image.tmdb.org/t/p/w800/"+ themoviedb.results[0].poster_path,
+                image_url: "https://image.tmdb.org/t/p/w800/"+ themoviedb.results[0].poster_path === 'N/A' ? 'http://placehold.it/350x150' : "https://image.tmdb.org/t/p/w800/"+ themoviedb.results[0].poster_path,
 
                 buttons: [{
                   type: 'postback',
